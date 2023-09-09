30daf237dfc24f2a1af3ef15fa430e8ef54448f6
sachinsomanwyd <sachinsomanwyd@gmail.com>
2023-09-09 12:59:14 +0530

    added calc.py

---
+++ calc.py
@@ -0,0 +1,12 @@
+print("Enter the numbers\n")
+a = int(input())
+b = int(input())
+sum = a + b
+diff = a-b
+prod = a * b
+quot = a / b
+print("Sum =", sum)
+print("Difference =", diff)
+print("Product =", prod)
+print("Quotient =", quot)
+
